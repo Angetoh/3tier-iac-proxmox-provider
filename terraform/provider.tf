864e420471c2d2ef34f2885a94c1efa5763cdbad
Angetoh <angetoh2@gmail.com>
2026-05-18 14:38:00 +0000

    update

---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc1" # Version stable pour Proxmox
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_tls_insecure = true
+  pm_debug        = true  # Active les logs si ça replante
+  pm_log_enable   = true
+  pm_log_file     = "terraform-plugin-proxmox.log"
+  pm_timeout = 600 # On donne 10 minutes à l'API pour chaque petite requête
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "pm_api_url" {
+  type = string
+}
+
+variable "pm_api_token_id" {
+  type = string
+}
+
+variable "pm_api_token_secret" {
+  type      = string
+  sensitive = true # hide secret on terraform plan/apply
+}
+
+
+variable "vm_user" {
+
+  type = string
+  
+}
+
+variable "sshkeys" {
+  type = string
+  sensitive = true
+}
+
+variable "Vms_ips" {
+  type = map(object({
+    ip = string
+    vmid = number
+  }))
+}
